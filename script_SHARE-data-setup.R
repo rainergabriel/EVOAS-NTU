d3a77c56a8d89ba3254298181a156d400723fc2b
rararainer <gabn@zhaw.ch>
2020-10-12 23:53:24 +0200

    Basics of setting up the data

---
+++ script_SHARE-data-setup.R
@@ -0,0 +1,53 @@
+#setting up the SHARE data
+
+library(foreign)
+
+wave7.hh <-
+  read.spss(
+    file = "/Users/rainer/Dropbox/Documents/Work/ACTIVE/ZHAW/3_Research-Projects/EVOAS-Team/2_Daten und Analysen/Other-Datasets-Raw-Data/SHARE RawData/sharew7_rel7-1-0_ALL_datasets_spss/sharew7_rel7-1-0_hh.sav",
+    to.data.frame = TRUE,
+    reencode = "UTF-8",
+    use.missings = TRUE
+  )
+
+wave7.tech <-
+  read.spss(
+    file = "/Users/rainer/Dropbox/Documents/Work/ACTIVE/ZHAW/3_Research-Projects/EVOAS-Team/2_Daten und Analysen/Other-Datasets-Raw-Data/SHARE RawData/sharew7_rel7-1-0_ALL_datasets_spss/sharew7_rel7-1-0_technical_variables.sav",
+    to.data.frame = TRUE,
+    reencode = "UTF-8",
+    use.missings = TRUE
+  )
+
+wave7.gv.housing <-
+  read.spss(
+    file = "/Users/rainer/Dropbox/Documents/Work/ACTIVE/ZHAW/3_Research-Projects/EVOAS-Team/2_Daten und Analysen/Other-Datasets-Raw-Data/SHARE RawData/sharew7_rel7-1-0_ALL_datasets_spss/sharew7_rel7-1-0_gv_housing.sav",
+    to.data.frame = TRUE,
+    reencode = "UTF-8",
+    use.missings = TRUE
+  )
+
+wave7.basics <-
+  read.spss(
+    file = "/Users/rainer/Dropbox/Documents/Work/ACTIVE/ZHAW/3_Research-Projects/EVOAS-Team/2_Daten und Analysen/Other-Datasets-Raw-Data/SHARE RawData/sharew7_rel7-1-0_ALL_datasets_spss/sharew7_rel7-1-0_cv_r.sav",
+    to.data.frame = TRUE,
+    reencode = "UTF-8",
+    use.missings = TRUE
+  )
+
+
+
+# Looking at the data -----------------------------------------------------
+
+names(wave7.tech)
+names(wave7.gv.housing)
+
+# Merge -------------------------------------------------------------------
+
+#prepare to merge by subsetting just the variables I need
+wave7.basics <-
+  subset(wave7.basics, select = c(mergeid, country, hhsize, age2017))
+wave7.hh <- subset(wave7.hh, select = c(mergeid, hh017e))
+
+df <- merge(wave7.basics, wave7.hh, by="mergeid")
+
+
